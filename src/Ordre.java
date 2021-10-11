5bdf055c09a1ec57d5d119464873e865d79e8579
Baeist <lars18n2@stud.kea.dk>
2021-10-11 16:41:29 +0200

    Version 1.1 added some classes

---
+++ src/Ordre.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Ordre {
+    private String kommentar;       // tror bedre hvis kommentar er i ordre oprettelsen, men ikke instantieringen
+    private int ordreID;
+    // private int day;                // angive dato giver mere mening når man starter programmet, så bare dato stamp fra main måske
+    // private int month;
+    private int hour;               // pizza skal være klar kl
+    private int minute;
+    private String ordreStatus;
+
+    Scanner scanner = new Scanner(System.in);
+
+    ArrayList<Integer> pizza = new ArrayList<>();
+
+    public Ordre(ArrayList pizza, int hour, int minute){
+        // noget med pizza numrene fra arraylist
+        this.ordreID = ordreID++;
+        this.hour = hour;
+        this.minute = minute;
+
+    }
+
+
+
+
+}
