961635fc11556a247e81801f415e5a819c62186c
Baeist <lars18n2@stud.kea.dk>
2021-10-12 10:02:05 +0200

    Version 1.3 pizza class

---
+++ src/Pizza.java
@@ -0,0 +1,20 @@
+public class Pizza {
+
+
+    private String navn;
+    private int nummer;
+    private String ingredienser;
+    private int pris;
+    private boolean pizzaStatus;
+
+    // Constructore
+    public Pizza (String nummer, String navn, String ingredienser, String pris) {
+        this.nummer = nummer;
+        this.navn = navn;
+        this.ingredienser = ingredienser;
+        this.pris = pris;
+
+        // pizzaStatus.
+        boolean pizzaStatus;
+    }
+}
--- src/PizzaMenu.java
+++ src/PizzaMenu.java
@@ -1,2 +1,18 @@
 public class PizzaMenu {
+    PizzaMenu() {  //nummer, navn, ingredienser, pris
+        Pizza p1 = new Pizza("1", "Vesuvio", "tomatsauce, ost, skinke og oregano", "57");
+        Pizza p2 = new Pizza("2", "Amerikaner", "tomatsauce, ost, oksefars og oregano", "53");
+        Pizza p3 = new Pizza("3", "Cacciatore", "tomatsauce, ost, pepperoni og oregano", "57");
+        Pizza p4 = new Pizza("4", "Carbona", "tomatsauce, ost, kødsauce, spaghetti, cocktailpølser, oregano", "63");
+        Pizza p5 = new Pizza("5", "Dennis", "tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano", "65");
+        Pizza p6 = new Pizza("6", "Bertil", "tomatsauce, ost, bacon og oregano", "57");
+        Pizza p7 = new Pizza("7", "Silvia", "tomatsauce, ost, pepperoni, rød peber, løg, oliven og oregano", "61");
+        Pizza p8 = new Pizza("8", "Victoria", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", "61");
+        Pizza p9 = new Pizza("9", "Toronfo", "tomatsauce, ost, skinke, bacon, kebab, chili og oregano", "61");
+        Pizza p10 = new Pizza("10", "Capricciosa", "tomatsauce, ost, skinke, champignon og oregano", "61");
+        Pizza p11 = new Pizza ("11", "Hawai", "tomatsauce, ost, skinke, ananas og oregano", "61");
+        Pizza p12 = new Pizza ("12", "Le Blissola", "tomatsauce, ost, skinke, ananas og oregano", "61");
+        Pizza p13 = new Pizza ("13", "Venezia", "tomatsauce, ost, pepperoni, bacon, løg og oregano", "61");
+        Pizza p14 = new Pizza ("14", "Mafia", "tomatsauce, ost, pepperoni, bacon, løg og oregano", "61");
+    }
 }
